e115cc392a98e44eceff9d41bc002f603926ff2d
Mohammad Samiul Islam <forthright48@gmail.com>
2016-07-27 17:47:59 +0600

    Refactor: move routes related to homepage to controller

--- README.md
+++ README.md
@@ -6,7 +6,21 @@ Main target is to familiarize myself with express-cassandra package.
 
 # Type of Queries to support
 
+## Manga
+
 1. Show all manga sorted according to rank and rating.
 1. Show all manga sorted by name.
 1. Show manga with particular tag, sorted according to rank and rating.
 1. Show manga of particular artist sorted according to rank and rating.
+
+## User
+
+1. Find an user with a particular username
+
+# Next task
+
+1. Babel
+2. 404 and 500 support
+3. Session
+4. Custom Middle ware
+5. R&D linter for other IDE
---
+++ controllers/home.js
@@ -0,0 +1,132 @@
+'use strict';
+
+const express = require('express');
+const models = require('express-cassandra');
+
+const router = express.Router();
+
+router.get('/', home);
+router.get('/insert', getInsert);
+router.post('/insert', postInsert);
+router.get('/delete/:id', getDelete);
+router.get('/edit/:id', getEdit);
+router.post('/edit/:id', postUpdate);
+router.get('/author/:author', getAuthor);
+router.get('/tags/:tag', getTag);
+
+module.exports = {
+  addRouter(app) {
+    app.use('/', router);
+  }
+};
+
+
+/*******************************************************************************
+Implementation
+*******************************************************************************/
+function home(req, res, next) {
+  models.instance.Manga.find({}, function(err, all) {
+    if (err) next(err);
+    res.render('home', {
+      data: all
+    });
+  });
+}
+
+function getInsert(req, res) {
+  res.render('insert');
+}
+
+function parseBodyToModel(obj, body) {
+  obj.name = body.name;
+  obj.author = body.author;
+  obj.tags = body.tags.split(',');
+  obj.photoURL = body.photoURL;
+  obj.startingDate = models.datatypes.LocalDate.fromString(body.startingDate);
+  obj.completed = body.completed === '1';
+  obj.rank = parseFloat(body.rank);
+  obj.rating = parseFloat(body.rating);
+  obj.description = body.description;
+}
+
+function postInsert(req, res, next) {
+  let obj = {};
+  parseBodyToModel(obj, req.body);
+  const newRow = new models.instance.Manga(obj);
+
+  newRow.save(function(err) {
+    if (err) return next(err);
+    else return res.redirect('/');
+  });
+}
+
+function getDelete(req, res, next) {
+  const id = models.datatypes.Uuid.fromString(req.params.id);
+
+  models.instance.Manga.findOne({
+    id
+  }, function(err, row) {
+    if (err) return next(err);
+    row.delete(function(err) {
+      if (err) return next(err);
+      return res.redirect('/');
+    });
+  });
+}
+
+function getEdit(req, res, next) {
+  const id = models.datatypes.Uuid.fromString(req.params.id);
+
+  models.instance.Manga.findOne({
+      id
+    },
+    function(err, row) {
+      if (err) return next(err);
+      res.render('edit', {
+        data: row
+      });
+    });
+}
+
+function postUpdate(req, res, next) {
+  const id = models.datatypes.Uuid.fromString(req.params.id);
+  models.instance.Manga.findOne({
+    id
+  }, function(err, row) {
+    if (err) return next(err);
+    parseBodyToModel(row, req.body);
+    row.save(function(err) {
+      if (err) return next(err);
+      return res.redirect('/');
+    });
+  });
+}
+
+function getAuthor(req, res, next) {
+  const author = req.params.author;
+  models.instance.Manga.find({
+    author
+  }, {
+    materialized_view: 'author_search'
+  }, function(err, all) {
+    if (err) return next(err);
+    return res.render('home', {
+      data: all
+    });
+  });
+}
+
+function getTag(req, res, next) {
+  const tag = req.params.tag;
+
+  models.instance.Manga.find({
+    tags: {
+      $contains: tag
+    }
+  }, function(err, all) {
+    if (err) return next(err);
+    return res.render('home', {
+      data: all
+    });
+  });
+}
