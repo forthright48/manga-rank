109cacbfe5adbf742bb3e1428e9fb190e2c81b81
Mohammad Samiul Islam <forthright48@gmail.com>
2016-07-28 18:29:33 +0600

    Relocate logger to forthright48 module

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const logger = require('./logs/winston.js');
+const logger = require('forthright48/logger');
 
 const app = express();
 const server = require('http').createServer(app);
